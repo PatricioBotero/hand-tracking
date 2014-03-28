56207d5b9074aa82d0b284dafb9578a63bf52cd1
patrick.buchter <patrick.buchter@hslu.ch>
2014-03-28 09:46:26 +0100

    change folder structure

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "Hand Tracking",
+    version = "0.0.1",
+    author = "Patrick Buchter",
+    author_email = "patrick.buchter@gmail.com",
+    description = ("tracking a hand with particle filter "
+                   "and the bhattacharyya distance as likelihood function."),
+    license = "BSD",
+    keywords = "tracking, particle filter, webcam",
+    url = "",
+    packages=['src', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
